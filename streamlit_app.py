702f578fd12f94329bbfd33b8724aa7fe767a25e
julianburs2025 <julian.burs@ahs.ca>
2025-03-07 16:15:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 
@@ -52,4 +53,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
    
-import requests
+
