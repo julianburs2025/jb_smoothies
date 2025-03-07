26cc230d7943feb6d16a39c8f39984d1613b42c6
julianburs2025 <julian.burs@ahs.ca>
2025-03-07 14:52:46 -0700

    Update streamlit_app.py
    connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 helpful_links = [
     "https://docs.streamlit.io",
