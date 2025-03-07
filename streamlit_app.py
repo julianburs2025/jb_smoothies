78c2b532d3c49b969aece740514f9e6fba1a32f8
julianburs2025 <julian.burs@ahs.ca>
2025-03-07 15:58:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-
 helpful_links = [
     "https://docs.streamlit.io",
     "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
@@ -23,6 +20,9 @@ st.write(
 # ord_filled = ("False")
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be: ", name_on_order)
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Get the current credentials
 session = get_active_session()
