d1c1a3db00d5d8e86ebacde179ee2cf9ab1b110a
julianburs2025 <julian.burs@ahs.ca>
2025-03-07 16:15:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,9 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
    # st.write(ingredients_string)  
    
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order )
@@ -50,6 +53,3 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
    
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
